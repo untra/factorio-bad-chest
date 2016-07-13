6cca43460916cda362a1efbcb4cddf60befaa762
Thomas Deal <justarandomgeek@gmail.com>
2016-07-13 11:44:15 -0400

    Draft frame descriptions and skeleton code for digitizer

--- digitizer.lua
+++ digitizer.lua
@@ -1,3 +1,101 @@
+--[[
+
+Commands 
+
+Reading a print leaves it in the input inventory
+in  blueprint: =1 Read print
+out blueprint: =1 successfully read, loaded print for editing 
+out blueprint: =-1 read failed
+
+Writing a print destroys the input print and creates a new print in the output based on the edits made
+in  blueprint: =2 Write print (move input->output inventory)
+out blueprint: =2 print written 
+out blueprint: =-2 write failed (still in buffer)
+
+Writing to a print
+in  blueprint: =4 Write content (combine with Report message / gets no response on success)
+out blueprint: =-4 write failed
+
+in  : =1 Request stats
+out S: Report print stats: S=1 0=#entities, 1=#tiles, 2=#icons, blue=#wholeprint
+in  I: Request icon: I=icon number
+out I: Report icon: I=icon number, $iconsignal=1
+in  T: Request tile: T=tile number
+out T: Report tile: T=tile number, X,Y, $tilesignal=1
+in  E: Request entity: E=entity number
+out E: Report entities: E=enity number, X,Y, $entitysignal=1, R=hasrecipe, C=#connections, F=#filters?, modules as count, ???
+in  C: Request entity-connections: C=entity number,0=connection number
+out C: Report entity-connections: C=entity number,0=connection number,1=remote entity,2=remote connection port
+in  R: Request recipe: R=entity number
+out R: Report recipe: R=entity number,X,Y $inputs=1, $output=2 (if something is both, =3)
+in  F: Request filters: F=entity number
+out F: Report filters: F=entity number, signals=filters
+in  ?: Request control behaviror: ?=entity number
+out ?: Report control behavior: ?= entity number, $type=1,type-specific data.
+in  ?: Request label: ?=character number
+out ?: Report label: ?=character number,letter=1
+
+print total size as serialized dump:
+    1 stats
+  0-4 icons
+    n label
+    n tiles
+    n entities
+    n recipies
+    n filters
+    n control behaviors
+    n wire connections 
+--]]
+
+
+
+local function readPrint(digitizer,signals)
+  --TODO: Read from input stack to digitizerprints[digitizer.unit_number], destroy print?
+end
+
+local function writePrint(digitizer,signals)
+  --TODO: Assemble a print in the output slot and 
+end
+
+local function bpTileFromSignals(digitizer,signals)
+  if not global.digitizerscripts[digitizer.unit_number] then return end
+  local tiles = global.digitizerscripts[digitizer.unit_number].get_blueprint_tiles()
+  
+end
+
+local function bpSignalsFromTile(digitizer,signals)
+  if not global.digitizerscripts[digitizer.unit_number] then return end
+end
+
+local function bpEntityFromSignals(digitizer,signals)
+  if not global.digitizerscripts[digitizer.unit_number] then return end
+end
+
+local function bpSignalsFromEntity(digitizer,signals)
+  if not global.digitizerscripts[digitizer.unit_number] then return end
+end
+
+local function bpConnectionFromSignals(digitizer,signals)
+  if not global.digitizerscripts[digitizer.unit_number] then return end
+end
+
+local function bpSignalsFromConnection(digitizer,signals)
+  if not global.digitizerscripts[digitizer.unit_number] then return end
+end
+
+
+
+
+
+local function bpEntityFromSignal(digitizer,signals)
+end
+local function bpTileFromSignal(digitizer,signals)
+end
+
+
+
+
+
 local function onTickDigitizer(digitizer)
   -- Check chest inventory for blueprint
   local digitizerInventory = digitizer.get_inventory(defines.inventory.chest)
@@ -21,6 +119,8 @@ local function onTickDigitizer(digitizer)
   
 end
 
+
+
 local function onTickDigitizers(event)
   if global.digitizers then
     for _,digitizer in pairs(global.digitizers) do
@@ -38,6 +138,7 @@ local function onBuiltDigitizer(event)
   if not ent or not ent.valid then return end
   if ent.name == "blueprint-digitizer" then 
     if not global.digitizers then global.digitizers={} end
+    if not global.digitizerprints then global.digitizerprints={} end
     table.insert(global.digitizers,ent)
   end
 end
