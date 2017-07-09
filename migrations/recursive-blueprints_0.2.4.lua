8ae21e44ac7880368b7e05dd6a65f1a4588739a5
DaveMcW <davemcw@gmail.com>
2017-07-09 11:05:53 -0400

    Remove printer and digitizer.

--- migrations/recursive-blueprints_0.2.4.lua
+++
@@ -1,5 +0,0 @@
-for i, force in pairs(game.forces) do 
- force.reset_recipes()
- force.reset_technologies()
- force.recipes["blueprint-digitizer"].enabled = force.technologies["recursive-blueprints"].researched
-end
