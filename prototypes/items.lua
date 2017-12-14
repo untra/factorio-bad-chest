b1ca5673aa801a53dd00e60b342e3619bb4d1fb9
DaveMcW <davemcw@gmail.com>
2017-12-13 20:41:04 -0500

    add icon_size

--- prototypes/items.lua
+++ prototypes/items.lua
@@ -3,10 +3,11 @@ data:extend{
     type = "item",
     name = "blueprint-deployer",
     icon = "__recursive-blueprints__/graphics/blueprint-deployer-icon.png",
+    icon_size = 32,
     flags = {"goes-to-quickbar"},
     subgroup = "tool",
     order = "c[automated-construction]-c[blueprint-deployer]",
     place_result = "blueprint-deployer",
-    stack_size = 50
+    stack_size = 50,
   },
 }
