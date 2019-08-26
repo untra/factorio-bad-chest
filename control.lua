60bcc5cd13de1fa3c9a3b4bdbd41a0243e0a7b5c
DaveMcW <davemcw@gmail.com>
2019-08-26 00:21:58 -0400

    Now supports on_entity_cloned.

--- control.lua
+++ control.lua
@@ -37,7 +37,7 @@ function on_mods_changed()
 end
 
 function on_built(event)
-  local entity = event.created_entity or event.entity
+  local entity = event.created_entity or event.entity or event.destination
   if not entity or not entity.valid then return end
   if entity.name == "blueprint-deployer" then
     table.insert(global.deployers, entity)
